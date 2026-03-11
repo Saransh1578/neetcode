f54b65c9edbbdc199c144f750dc0f84b622a13db
Saransh Pandey <145375381+Saransh1578@users.noreply.github.com>
2026-03-11 14:58:00 +0530

    March 11, 2026

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,24 @@
+// Leetcode Problem 238
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        
+        
+        int pre = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = pre;      
+            pre *= nums[i];    
+        }
+        
+        
+        int pos = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= pos;  
+            pos *= nums[i];    
+        }
+        
+        return res;
+    }
+}
